02415928b66d80bd809d35ef43298f708730e5a5
dlutzy <36352458+tlwzzy@users.noreply.github.com>
2022-06-23 10:44:50 +0800

    Create ubuntu_tsinghua_root.sh

---
+++ 18.04/ubuntu_tsinghua_root.sh
@@ -0,0 +1,3 @@
+apt update && apt install -y apt-transport-https ca-certificates wget 
+wget https://raw.fastgit.org/tlwzzy/MyProject/master/18.04/sources.list
+mv sources.list /etc/apt/sources.list
